61eba6cb17528e957277ef2ab7ca6f76f47e4906
canarimiguel <94536599+canarimiguel@users.noreply.github.com>
2024-02-05 20:43:24 +0000

    Add files via upload

---
+++ Reto 1.py
@@ -0,0 +1,15 @@
+# 1º Ejercicio del RETO Python
+
+print ("Por favor introduzca los datos solicitados")
+print ("-------------------------------------------")
+
+print() # para que no salga todo seguido
+nombre = input("Nombre .............: ")
+apellidos = input("Apellidos...........: ")
+telefono = int(input("Teléfono............: "))
+mail = input("Correo electrónico..: ")
+
+# el programa le da la bienvenida al usuario:
+print() # para que no salga todo seguido
+print ("Hola" + " " + nombre + " " + apellidos + ", " + "en breve recibirás un correo en" + " " + mail)
+ 
